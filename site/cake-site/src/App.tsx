d8b77794392109f9b69a729d75310f282c580b98
Andres98200 <andrwtorres2003@gmail.com>
2025-07-02 23:22:21 +0200

    tailwind install and setup of the project

---
+++ site/cake-site/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-400">Hello Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
