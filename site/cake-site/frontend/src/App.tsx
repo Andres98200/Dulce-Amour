857ddb0898de07a99cdff2fc3251abe7e03f7006
Andres98200 <andrwtorres2003@gmail.com>
2025-07-03 23:38:05 +0200

    creation of the frontend folder

---
+++ site/cake-site/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline text-blue-400 pb-8">Testing the branch!</h2>
+      <h1 className="text-3xl font-bold underline text-blue-400">Hello Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
