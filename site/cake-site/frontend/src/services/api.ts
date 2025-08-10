1f39374eb63ee3433dfd0455fcf4021cbf5a99d7
Andres98200 <andrwtorres2003@gmail.com>
2025-08-10 22:36:57 +0200

    feat(add) getproductbyId method for the front and use of .env to be more secure

--- site/cake-site/frontend/src/services/api.ts
+++ site/cake-site/frontend/src/services/api.ts
@@ -1,10 +1,19 @@
 import type { Product } from "../types/Product";
-
+const API_URL = import.meta.env.VITE_API_URL;
 // get all products
 export async function fetchData(): Promise<Product[]> {
-    const response = await fetch("http://localhost:4000/api/products/All-Products");
-    if (!response.ok) throw new Error("Erreur réseau");
+    const response = await fetch(`${API_URL}/All-Products`);
+    if (!response.ok) throw new Error("Error while fetching all the products");
     const data = (await response.json()) as { message: string; products: Product[] };
     return data.products;
   }
-  
+
+// get a product by id
+
+export async function getProductbyId(productId:string): Promise<Product> {
+    const response = await fetch(`${API_URL}/${productId}`);
+    if (!response.ok) throw new Error("Error while fetching the product you request");
+    const data = (await response.json()) as {message: string; product: Product};
+    console.log("Product successfully fetched", data.product)
+    return data.product;
+}
