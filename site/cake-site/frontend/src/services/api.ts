08900fb8cb93ed5b0a76b04d18a0111fe954923a
Andres98200 <andrwtorres2003@gmail.com>
2025-08-10 23:28:40 +0200

    feat(add) add of the getProducById method on the home page

--- site/cake-site/frontend/src/services/api.ts
+++ site/cake-site/frontend/src/services/api.ts
@@ -1,10 +1,11 @@
-import type { Product } from "../types/Product";
+import type { Product, ProductListResponse, ProductResponse } from "../types/Product";
 const API_URL = import.meta.env.VITE_API_URL;
 // get all products
 export async function fetchData(): Promise<Product[]> {
     const response = await fetch(`${API_URL}/All-Products`);
     if (!response.ok) throw new Error("Error while fetching all the products");
-    const data = (await response.json()) as { message: string; products: Product[] };
+    const data = (await response.json()) as ProductListResponse;
+    console.log("All products successfully fetched", data.products)
     return data.products;
   }
 
@@ -13,7 +14,7 @@ export async function fetchData(): Promise<Product[]> {
 export async function getProductbyId(productId:string): Promise<Product> {
     const response = await fetch(`${API_URL}/${productId}`);
     if (!response.ok) throw new Error("Error while fetching the product you request");
-    const data = (await response.json()) as {message: string; product: Product};
+    const data = (await response.json()) as ProductResponse;
     console.log("Product successfully fetched", data.product)
     return data.product;
 }
