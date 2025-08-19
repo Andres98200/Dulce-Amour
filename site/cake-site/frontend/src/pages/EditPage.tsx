ed3b0575766d8621a88df9a02237506c2b98ed0c
Andres98200 <andrwtorres2003@gmail.com>
2025-08-19 23:15:10 +0200

    edit pagz

--- site/cake-site/frontend/src/pages/EditPage.tsx
+++ site/cake-site/frontend/src/pages/EditPage.tsx
@@ -4,9 +4,7 @@ export default function EditPage() {
     const { t } = useTranslation();
     return(
         <div className="flex justify-center items-center min-h-screen">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-                {t("Edit")}
-            </button>
+            <h1 className="flex justify-center mb-8">{t("Welcome back Laura")}</h1>
         </div>
     )
 }
