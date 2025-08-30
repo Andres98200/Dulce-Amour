612e41d43c49645deee6fe5d709973f39ef53645
Andres98200 <andrwtorres2003@gmail.com>
2025-08-30 20:32:05 +0200

    feat(fix): fix bug with cloudinary stock of the images

--- site/cake-site/backend/src/utils/cloudinaryConfig.ts
+++ site/cake-site/backend/src/utils/cloudinaryConfig.ts
@@ -1,20 +1,19 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME !,
-    api_key: process.env.CLOUDINARY_API_KEY !,
-    api_secret: process.env.CLOUDINARY_API_SECRET !,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async (req, file) => ({
-      folder: 'Cake_Site_images',
-      allowed_formats: ['jpg', 'jpeg', 'png'],
-      public_id: file.originalname.split('.')[0],
-    }),
-  });
-  
-  export { cloudinary, storage };
+  cloudinary,
+  params: async (req, file) => ({
+    folder: "Cake_Site_images",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    public_id: `${Date.now()}-${file.originalname}`,
+  }),
+});
+
+export { cloudinary, storage };
